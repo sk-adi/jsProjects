bd419c44a6fdcd24f1318cb0f9b781d3b2a71480
aditya <176797545+sk-adi@users.noreply.github.com>
2024-12-18 14:38:52 +0530

    change variable name from top to topsection

--- ResponsiveNavbar/script.js
+++ ResponsiveNavbar/script.js
@@ -1,12 +1,9 @@
 const menutoggle=document.getElementsByClassName("menutoggle")[0]
 const menu=document.getElementsByClassName("rightPart")[0]
-const top=document.getElementsByClassName("top")[0]
+const topsection=document.getElementsByClassName("topsection")[0]
 
 menutoggle.addEventListener("click",()=>{
     menu.classList.toggle("mystyle")
-    // top.classList.toggle("topstyle")
+    topsection.classList.toggle("topstyle")
 
 })
-
-
-
