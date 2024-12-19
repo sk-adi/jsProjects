066d4ed715978cfaddc6cd09db4045f99e22e6e3
aditya <176797545+sk-adi@users.noreply.github.com>
2024-12-19 12:29:00 +0530

    completed basic version of my todolist project

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,12 +1,44 @@
-const item=document.getElementById('item')
-const add=document.getElementById('add')
+const item = document.getElementById('item')
+const add = document.getElementById('add')
+const clearList=document.getElementById("clearList")
+const thelist=document.getElementById("thelist")
 
-let myList=[]
-add.addEventListener("click",()=>{
-    addeditem=item.value
+
+const myList = []
+add.addEventListener("click", () => {
+    addeditem = item.value
     myList.push(addeditem)
-    alert(addeditem)
     console.log(addeditem)
+    addingelement()
 })
 
-console.log(myList)
+
+function addingelement(){
+
+    const parel = document.getElementById('thelist')
+    let newel = document.createElement('li')
+    newel.textContent=myList.at(-1).trim()
+    parel.append(newel)
+    item.value=''
+
+}
+
+
+clearList.addEventListener('click',()=>{
+    while (thelist.firstChild){
+        thelist.removeChild(thelist.firstChild)
+    }
+
+    myList=[]
+})
+
+
+document.addEventListener('keydown',(event)=>{
+    if(event.key==='Enter'){
+        addeditem = item.value
+        myList.push(addeditem)
+        console.log(addeditem)
+        addingelement()
+    }
+})
+
